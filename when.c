19d93b4f6baea5733ca6c15233c5dce67ed7f425
FrancoisPog <francoispoguet@gmail.com>
2020-04-16 12:27:08 +0200

    update : when

--- when.c
+++ when.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
-int main(){
-    time_t now = time(NULL);
-    printf("%s",ctime(&now));
+int main(int argc, char *argv[]){
+    char *usage = "Usage : ./when time";
+
+    if(argc != 2){
+        fprintf(stderr,"Error : One and only one time must be specified\n%s\n",usage);
+        return 1;
+    }
+
+    time_t time = atol(argv[1]);
+    printf("%s",ctime(&time));
     return 0;
 }
